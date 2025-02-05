60e3ff3bcc9132ab72dd5f42057bab6b94ce13df
Laura <la.alvaradoo1@uniandes.edu.co>
2025-02-05 09:35:00 -0500

    Revert "Merge remote-tracking branch 'origin/Est-3'"
    This reverts commit 979511d5dec7e5bb409791d7b8861330da01fc26, reversing
    changes made to 9fdd4a6fc8ea993f574a37e05fbfef51d024a886.

--- App/logic.py
+++ App/logic.py
@@ -133,9 +133,9 @@ def load_books_tags(catalog, filename):
     :rtype: int
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    catalog = control["model"]
-    booksfile = os.path.join(cf.data_dir, filename)
-    catalog = model.addBookTags(catalog, booksfile)
+    catalog =control["model"]
+    booksfile =os.path.join(cf.data_dir, filename)
+    catalog =model.addBookTags(catalog, booksfile)
     return model.bookTagSize(catalog)
 
 
@@ -144,7 +144,7 @@ def first_book(catalog):
     Devuelve el primer libro del catalogo
     """
     # TODO: Mods Est-3 en el Lab 2
-    return set.get_first_element(catalog["books"])
+    pass
 
 
 def last_book(catalog):
@@ -152,7 +152,7 @@ def last_book(catalog):
     Devuelve el ultimo libro del catalogo
     """
     # TODO: Mods Est-3 en el Lab 2
-    return set.get_last_element(catalog["books"])
+    pass
 
 # Funciones para la manipulacion de los datos
 
@@ -199,22 +199,21 @@ def add_book_tag(catalog, booktag):
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
     set.add_element(catalog["book_tags"], booktag)
     return catalog
-    set.add_element(catalog["book_tags"], booktag)
-    return catalog
+    pass
 
 
 # Funciones de consulta
 
 def book_size(catalog):
     # TODO: Mods de Est-3 en el Lab 2
-    return set.size(catalog["books"])
+    pass
 
 
 def tag_size(catalog):
     # TODO: Mods de Est-3 en el Lab 2
-    return set.size(catalog["tags"])
+    pass
 
 
 def book_tag_size(catalog):
     # TODO: Mods de Est-3 en el Lab 2
-    return set.size(catalog["book_tags"])
+    pass
